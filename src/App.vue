1ea51d93ca7e6e2f7e99e6f9addf33ec92286dda
Susanta <inbox.susanta@gmail.com>
2021-02-10 21:37:27 +0530

    Showing a Results Component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
 <h1>Ninja Reaction Timer</h1>
 <button @click="start" :disabled="isPlaying">play</button>
 <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-<p v-if="showResults">Reaction time: {{ score }}</p>
+<!-- <p v-if="showResults">Reaction time: {{ score }}</p> -->
+<Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
